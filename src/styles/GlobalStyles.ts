9ae06f147058a41604a693ac37e0d900f954ba21
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-07-02 17:17:44 -0300

    style: add cores e fonte

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,30 +5,49 @@ export const GlobalStyles = createGlobalStyle`
         --light-black: #343434;
         --mid-black: #313131;
         --grey: #494949;
+        --light-grey: #5F5F5F;
+        --mid-grey: #9a9a9a;
         --red-orange: #A54D27;
+
+        --topbar: #2B2B2B;
+        --topbar-title: #71BB00;
+        --topbar-subtitle: var(--light-grey);
+        --topbar-border-bottom: #232323;
         
 
         background-color: var(--light-black);
         box-sizing: border-box;
-        margin: 3rem;
-
-        font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-        line-height: 1.5;
-        font-weight: 400;
-
-        font-synthesis: none;
-        text-rendering: optimizeLegibility;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-
-        @media screen and (min-width: 992px) {
-            margin: 0 10rem;
-        }
     }
 
     * {
         margin: 0;
         padding: 0;
+
+        font-family: "Archivo", sans-serif, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+        line-height: 1.5;
+        font-weight: 400;
+        font-optical-sizing: auto;
+
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    input, textarea {
+        background: var(--grey);
+        border: transparent;
+        border-radius: 5px;
+        padding: 5px 10px;
+        color: var(--mid-grey);
+        font-size: 0.72rem;
+        text-align: start;
+
+        &::placeholder {
+            color: var(--mid-grey);
+            font-size: 0.73rem;
+            text-align: start;
+
+        }
     }
 `;
