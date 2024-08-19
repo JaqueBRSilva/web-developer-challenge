dc77d93179ee3342de6cabd2fb59e0df5148dec7
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-08-19 17:35:23 -0300

    feat: adicionado infos dos cards postados

---
+++ src/data/postsData.ts
@@ -0,0 +1,23 @@
+const IMG_1 = 'https://images.unsplash.com/photo-1508313157893-34fe6176c189'
+const IMG_2 = 'https://images.unsplash.com/photo-1473116763249-2faaef81ccda'
+
+const TEXT_1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus."
+const TEXT_2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit."
+
+const AUTHOR = 'Manuela Oliveira'
+
+export const POSTS_DATA = [
+    {
+        id: '1',
+        cardPublishedImage: IMG_1,
+        cardPublishedText: TEXT_1,
+        cardPublishedAuthor: AUTHOR
+    },
+    
+    {
+        id: '2',
+        cardPublishedImage: IMG_2,
+        cardPublishedText: TEXT_2,
+        cardPublishedAuthor: AUTHOR
+    },
+]
