63555e8080f7a581c9b7a15cb968f26de9757204
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-08-21 23:58:29 -0300

    feat: adicionado renderização de lista em 'FeedPosts'

--- src/components/FeedPosts/FeedPosts.tsx
+++ src/components/FeedPosts/FeedPosts.tsx
@@ -1,15 +1,33 @@
-import CardsPublishedList from "../CardsPublishedList/CardsList"
+import { useState } from "react"
+import { POSTS_DATA } from "../../data/postsData"
+import { CardPublished_Props } from "../../types/Post"
+import CardsList from "../CardsList/CardsList"
 import { FeedPostsMain, FeedPostsTitleSection } from "./styles"
 
 const FeedPosts: React.FC = () => {
+    const [loadedPosts, setLoadedPosts] = useState(POSTS_DATA)
+
+    const handleDeleteCardPost = (postID: string) => {
+        let deletePostSelected = loadedPosts.filter((card: { id: string }) => card.id != postID)
+        return setLoadedPosts(deletePostSelected)
+    }
+
     return (
         <FeedPostsMain>
             <FeedPostsTitleSection>
                 Feed
             </FeedPostsTitleSection>
-
-            <CardsPublishedList />
-
+            {
+                loadedPosts.map((cardData: CardPublished_Props) => (
+                    <CardsList
+                        key={cardData.id}
+                        deletePostButton={() => handleDeleteCardPost(cardData.id)}
+                        cardPublishedImage={cardData.cardPublishedImage}
+                        cardPublishedText={cardData.cardPublishedText}
+                        cardPublishedAuthor={cardData.cardPublishedAuthor}
+                    />
+                ))
+            }
         </FeedPostsMain>
     )
 }
